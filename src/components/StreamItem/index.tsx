a236de83a22eda0c60b99297f77e215efb4cc882
Anderson Rodrigues <anddersonrds@gmail.com>
2020-09-02 08:41:55 -0300

    add components ChannelList, StreamList & StreamItem

---
+++ src/components/StreamItem/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import streamThumbnail from '../../images/stream_thumbnail.jpg';
+
+import {
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from './styles';
+
+const StreamItem: React.FC = () => {
+  return (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>anddersonrds</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Front-end com Next.js, Chakra UI e GraphQL
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>
+            Science & Technology
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Typescript</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  );
+};
+
+export default StreamItem;
