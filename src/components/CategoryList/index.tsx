4cd4571bc2e05f8c91a9c348b4776552cee9425a
Anderson Rodrigues <anddersonrds@gmail.com>
2020-09-02 08:39:48 -0300

    add component categoryList

---
+++ src/components/CategoryList/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import CategoryItem from '../CategoryItem';
+
+import data from './data';
+
+import { List } from './styles';
+
+const CategoryList: React.FC = () => {
+  return (
+    <List>
+      {data.map(item => (
+        <CategoryItem key={item.name} item={item} />
+      ))}
+    </List>
+  );
+};
+
+export default CategoryList;
